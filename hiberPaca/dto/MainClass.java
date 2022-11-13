40696459c92d3580de265347b6b36ae7b1b27761
Kama Jo <jonathantanga97@gmail.com>
2022-11-14 00:05:46 +0300

    I have changed the create property to update in the cfg.xml file

--- hiberPaca/dto/MainClass.java
+++ hiberPaca/dto/MainClass.java
@@ -10,8 +10,8 @@ public class MainClass {
 	 
 		ModelClass obj = new ModelClass();
 		ModelClass obj2 = new ModelClass();
-		ModelClass obj3 = new ModelClass();
-		ModelClass obj4 = new ModelClass();
+		
+		ModelClass obj5 = new ModelClass();
 		
 		obj.setModelId("t1");
 		obj.setName("Kafumisi");
@@ -21,21 +21,17 @@ public class MainClass {
 		obj2.setName("Kagogwa");
 		obj2.setTribe("Muganda");
 		
-		obj3.setModelId("t32");
-		obj3.setName("Sabula");
-		obj3.setTribe("Muchwezi");
-		
-		obj4.setModelId("t42");
-		obj4.setName("Kayingini");
-		obj4.setTribe("Muganda");
+		obj5.setModelId("t7");
+		obj5.setName("Mudaliso");
+		obj5.setTribe("Muganda");
 		
 		SessionFactory Fac = new Configuration().configure().buildSessionFactory();
 		Session session = Fac.openSession();
 		session.beginTransaction();
 		session.save(obj);
 		session.save(obj2);
-		session.save(obj3);
-		session.save(obj4);
+		
+		session.save(obj5);
 		session.getTransaction().commit();
 		
 		
